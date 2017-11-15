0f15816d8ea64de13c9fa453eb661325ceb2785b
KCZhang <kczhang@thoughtworks.com>
2017-11-15 17:14:28 +0800

    delete already code

--- src/main/java/com/example/order/OrderApplication.java
+++ src/main/java/com/example/order/OrderApplication.java
@@ -2,11 +2,8 @@ package com.example.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -14,8 +11,6 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableEurekaClient
-@EnableFeignClients
 @EnableSwagger2
 public class OrderApplication {
 
